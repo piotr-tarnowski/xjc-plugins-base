ae01d9cbc5e449277c24b90bac9465cfcaa0ba74
Piotr Tarnowski <Piotr.Tarnowski@performgroup.com>
2017-06-23 15:58:24 +0200

    JAXBFilterXJCPlugin first approach finished.

--- src/main/java/com/devontrain/jaxb/plugins/JAXBFilterXJCPlugin.java
+++ src/main/java/com/devontrain/jaxb/plugins/JAXBFilterXJCPlugin.java
@@ -20,54 +20,66 @@ import java.util.Iterator;
 @XJCPlugin(name = "Xfilter")
 public class JAXBFilterXJCPlugin extends XJCPluginBase {
 
-    private static final String CURRENT_PACKAGE = "*";
+  private static final String CURRENT_PACKAGE = "*";
 
-    @XJCPluginProperty(defaultValue = CURRENT_PACKAGE)
-    private String pckg = CURRENT_PACKAGE;
+  @XJCPluginProperty(defaultValue = CURRENT_PACKAGE)
+  private String pckg = CURRENT_PACKAGE;
 
-    @XJCPluginCustomizations(uri = "http://common.jaxb.devontrain.com/plugin/filter-generator")
-    private interface coll {
-    }
+  @XJCPluginCustomizations(uri = "http://common.jaxb.devontrain.com/plugin/filter-generator")
+  private interface coll {
+  }
 
-    @Override
-    public boolean run(Outline omodel, Options opt, ErrorHandler errorHandler) throws SAXException {
+  @Override
+  public boolean run(Outline omodel, Options opt, ErrorHandler errorHandler) throws SAXException {
 
+    JCodeModel codeModel = omodel.getCodeModel();
 
-        JCodeModel codeModel = omodel.getCodeModel();
+    Iterator<? extends ClassOutline> iterator = omodel.getClasses().iterator();
+    while (iterator.hasNext()) {
+      ClassOutline classOutline = iterator.next();
 
-
-        Iterator<? extends ClassOutline> iterator = omodel.getClasses().iterator();
-        while (iterator.hasNext()) {
-            ClassOutline classOutline = iterator.next();
-
-            try {
-                JDefinedClass implClass = classOutline.implClass;
-                JDefinedClass newClass;
-                String newClassName = implClass.name() + "Filter";
-                if (pckg == CURRENT_PACKAGE) {
-                    JClassContainer parent = implClass.parentContainer();
-                    newClass = parent._class(newClassName);
-                } else {
-                    newClass = codeModel._class(pckg + "." + newClassName);
-                }
-                JClass adapter = codeModel.ref(XmlAdapter.class).narrow(implClass, implClass);
-                //TODO: inherit class from parameter
-                newClass._extends(adapter);
-                createIdentityMethod("unmarshal", newClass, implClass);
-                //TODO: rather then identity we shall invoke filter method
-                createIdentityMethod("marshal", newClass, implClass);
-            } catch (JClassAlreadyExistsException ex) {
-                ex.printStackTrace();
-            }
-
+      try {
+        JDefinedClass implClass = classOutline.implClass;
+        JDefinedClass newClass;
+        String newClassName = implClass.name() + "Filter";
+        if (pckg == CURRENT_PACKAGE) {
+          JClassContainer parent = implClass.parentContainer();
+          newClass = parent._class(JMod.PUBLIC + JMod.ABSTRACT, newClassName, ClassType.CLASS);
+        } else {
+          newClass = codeModel._class(JMod.PUBLIC + JMod.ABSTRACT, pckg + "." + newClassName, ClassType.CLASS);
         }
-        return true;
-    }
+        JClass adapter = codeModel.ref(XmlAdapter.class).narrow(implClass, implClass);
+        newClass._extends(adapter);
+        JFieldVar counter = newClass.field(JMod.PROTECTED, int.class, "counter");
+        createIdentityMethod("unmarshal", newClass, implClass);
 
-    private void createIdentityMethod(String name, JDefinedClass newClass, JDefinedClass implClass) {
-        JMethod method = newClass.method(JMod.PUBLIC, implClass, name);
-        JVar param = method.param(implClass, "v");
-        method.body()._return(param);
-        method.annotate(Override.class);
+        JMethod valid = newClass.method(JMod.PUBLIC, boolean.class, "valid");
+        valid.body()._return(counter.gt(JExpr.lit(0)));
+
+        JMethod test = newClass.method(JMod.PUBLIC + JMod.ABSTRACT, boolean.class, "test");
+        test.param(implClass, "entity");
+
+        JMethod marshal = newClass.method(JMod.PUBLIC, implClass, "marshal");
+        JVar param = marshal.param(implClass, "entity");
+        final JConditional condition = marshal.body()._if(JExpr.invoke(test).arg(param));
+        condition._then()._return(JExpr._null());
+        final JBlock block = condition._else();
+
+//        final JExpression incr = counter.incr();
+        block.directStatement("counter++;");
+        block._return(param);
+        marshal.annotate(Override.class);
+      } catch (JClassAlreadyExistsException ex) {
+        ex.printStackTrace();
+      }
     }
+    return true;
+  }
+
+  private void createIdentityMethod(String name, JDefinedClass newClass, JDefinedClass implClass) {
+    JMethod method = newClass.method(JMod.PUBLIC, implClass, name);
+    JVar param = method.param(implClass, "entity");
+    method.body()._return(param);
+    method.annotate(Override.class);
+  }
 }
